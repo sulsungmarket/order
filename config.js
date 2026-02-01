69cef021153421f71171e987b31b9ea06199285a
sulsungmarket <sulsungmarket@gmail.com>
2026-02-02 03:32:04 +0900

    Update config.js
    실 라이브 url적용

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // LIVE 환경 설정 (order/)
 window.__ENV__ = {
   TAG: "LIVE",
-  SCRIPT_URL: "https://script.google.com/macros/s/LIVE_GAS_URL/exec"
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzKhIH1smdYwZ7HLACMFKVTArkLfRqvkKlVMiBszBcvZf3n7fv_1KGPssyzMplyo-K6/exec";
 };
