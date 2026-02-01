39a9eef0efe665b9e697ddd86aab5b38f1677628
sulsungmarket <sulsungmarket@gmail.com>
2026-02-02 03:40:34 +0900

    Update config.js
    링크 세미콜론 삭제

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // LIVE 환경 설정 (order/)
 window.__ENV__ = {
   TAG: "LIVE",
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzKhIH1smdYwZ7HLACMFKVTArkLfRqvkKlVMiBszBcvZf3n7fv_1KGPssyzMplyo-K6/exec";
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzKhIH1smdYwZ7HLACMFKVTArkLfRqvkKlVMiBszBcvZf3n7fv_1KGPssyzMplyo-K6/exec"
 };
