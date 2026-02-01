7940d99a9a59735350c706205e295d1a04e5a35a
sulsungmarket <sulsungmarket@gmail.com>
2026-02-02 03:23:09 +0900

    Update config.js
    실제 dev링크 적용

--- dev/config.js
+++ dev/config.js
@@ -1,5 +1,5 @@
 // DEV 환경 설정 (order/dev/)
 window.__ENV__ = {
   TAG: "DEV",
-  SCRIPT_URL: "https://script.google.com/macros/s/DEV_GAS_URL/exec"
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz0SUKbp05CwUovG111sbzrBA00SdTRbEU2xPwvz9lCabUYOdZkJyJr9jaxucVojh43/exec"
 };
